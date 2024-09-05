3f3a2b0b4ba7e982b99ec36caa9cb9ec735130a7
Pratik Patil <154731963+PatilPratik16@users.noreply.github.com>
2024-09-05 05:21:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treats all the JS code as newer version
+
+
+// alert('hello')   ==>  we are using nodejs not a browser therefore it is not working
+
+//code readiability should be their
+console.log(30+3);
+
+let string = 'pratik';
+let boolean = true;
+let number = 2;
+let arr = [ true , '20' , 'krish' , [1 , 2 , 3 ]];  
+
+// null => is a stand alone value , it can be assigned
+// undefine => when value is not assigned to datatype
+
+
+console.log(typeof undefined)  // it is undefined
+console.log(typeof null)  // it is object
